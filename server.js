bee55faa3d3149a9c0ec26fae4f5fe0571f3286e
Wanchana Srisuko <wanchana_s@kingpower.com>
2019-08-24 17:18:09 +0700

    Initial project

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+const port = process.env.PORT || 3000
+const places = require("google-places-web").default; // instance of GooglePlaces Class;
+ 
+// Setup
+places.apiKey = "AIzaSyDqzWLn5dOhOIhzZN2kz-jePA0HM1vV-Sg";
+places.debug = false; // boolean;
+
+
+app.get('/', (req, res) => {
+  res.send('Hello')
+})
+
+app.get('/restaurant/bangsue', (req, res) => {
+    
+    var resultJson = {}
+
+    places.nearbysearch({
+        location: "13.828025,100.528100", // LatLon delimited by,
+        radius: "3",  // Radius cannot be used if rankBy set to DISTANCE
+        type: ["restaurant"], // Undefined type will return all types
+        //rankby: "distance" // See google docs for different possible values
+      })
+        .then(result => {
+            // result object
+            resultJson = JSON.parse(JSON.stringify(result));
+            
+            res.status(200).json(resultJson)
+        })
+        .catch(e => {
+            console.log(e)
+            res.status(404).send(String(e))
+        });
+})
+
+//For line message api
+app.post('/webhook', (req, res) => {
+    let reply_token = req.body.events[0].replyToken
+    reply(reply_token)
+    res.sendStatus(200)
+})
+
+//start sever
+app.listen(port, () => {
+    console.log('Start server at port '+port+'.')
+})
+
+function reply(reply_token) {
+    let headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 827c3ad3c3af19759ea628966c1eb3ac'
+    }
+    let body = JSON.stringify({
+        replyToken: reply_token,
+        messages: [{
+            type: 'text',
+            text: 'Hello'
+        },
+        {
+            type: 'text',
+            text: 'How are you?'
+        }]
+    })
+    request.post({
+        url: 'https://api.line.me/v2/bot/message/reply',
+        headers: headers,
+        body: body
+    }, (err, res, body) => {
+        console.log('status = ' + res.statusCode);
+    });
+}
+
